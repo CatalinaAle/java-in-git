5753b2bf83de9928d993e33b3f7edb658f3aed33
Catalina <cristea.catalina89@gmail.com>
2018-12-15 15:19:59 +0200

    second project

--- src/first/java/Main.java
+++ src/first/java/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("My first project in git ");
-        System.out.println("kkgkg");
+        System.out.println("kkg nllkg");
     }
 }
